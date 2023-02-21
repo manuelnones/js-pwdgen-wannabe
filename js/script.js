cb3d9fe7da39faa83417f38a92add684ac80024a
manuelnones <manuelnones99@yahoo.com>
2023-02-21 13:46:08 +0100

    fin exercise

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// - creazione variabile nome
+
+// - creazione variabile cognome
+
+// - creazione variabile colore preferito
+
+// - concatenazione variabili + numero scelto
+
+// ______________________________________________
+
+let nome = prompt ("Inserisci il tuo nome")
+
+
+let cognome = prompt ("Inserisci il tuo cognome")
+
+
+
+let colorePreferito = prompt ("Inserisci il tuo colore preferito")
+
+document.writeln (nome + cognome + colorePreferito + ("24"))
